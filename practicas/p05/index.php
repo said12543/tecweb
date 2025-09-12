ab32533aa9be8e3a86c77a1f0c305449a45446c3
PridedGnu <saidcesar118@gmail.com>
2025-09-11 23:11:05 -0600

    Práctica 5, parte 4

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -29,6 +29,7 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+        unset($a, $b, $c, $z);
     ?>
     <h2>Inciso  2</h2>
     <p>Proporciona los valores de $a, $b, $c como sigue:</p>
@@ -55,6 +56,8 @@
         echo "<h4>Descripción:</h4>";
         echo "<p>Antes de realizarla creeria que me saltaria algun tipo de error o que pasaría algo con la impresión de las segundas variables. Por un comentario del profesor consideré utilizar la función unset(_).</p>";
         echo "<p>Finalmente lo que sucedio fue que el valor de las segundas variables se sobreescribio sobre las primeras y la impresión de ambas fue correcta</p>";
+        unset($a, $b, $c, $z);
+
     ?>
     <h2>Inciso  3</h2>
     <p>Muestra el contenido de cada variable inmediatamente después de cada asignación,
@@ -69,27 +72,42 @@
     <p>$z[0] = “MySQL”;</p>
 
     <?php
+        echo "a: ";
         $a = "PHP5";
         var_dump($a);
-        echo "<br>";
+        echo "<br>z: ";
         $z[] = &$a;        
         var_dump($z);
-        echo "<br>";
+        echo "<br>b: ";
         $b = "5a version de PHP";
         var_dump($b);
-        echo "<br>";        
+        echo "<br>c: ";        
         $c = $b*10;
         var_dump($c);
-        echo "<br>";        
+        echo "<br>(Se muestra un warning debido a que se intenta multiplicar una cadena, aunque si se detecta el 5 por lo que posterior al aviso se muestra un 50)<br><br>a: ";        
         $a .= $b;
         var_dump($a);
-        echo "<br>";       
+        echo "<br>b: ";       
         $b *= $c;
         var_dump($b);
-        echo "<br>";        
+        echo "<br>(Sucede lo mismo, da 250 pero lanza un warning)<br>z: ";        
         $z[0] = "MySQL";
         var_dump($z);
         echo "<br><br>";
+        
+    ?>
+    <h2>Inciso  4</h2>
+    <p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
+    la matriz $GLOBALS o del modificador global de PHP.</p>
+    <?php
+        echo "\$a: " . $GLOBALS['a'] . "<br>";
+        echo "\$b: " . $GLOBALS['b'] . "<br>";
+        echo "\$c: " . $GLOBALS['c'] . "<br>";
+        echo "\$z: ";
+        print_r($GLOBALS['z']);
+        echo "<br>";
+        unset($a, $b, $c, $z);
+
     ?>
 </body>
 </html>
