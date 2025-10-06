5f562ca15701f82a0b63d821e13bb4b27dc9422c
PridedGnu <saidcesar118@gmail.com>
2025-10-05 21:22:29 -0600

    Actividad 5, ejercicio 7

--- actividades/Actividad 5/src/main.js
+++ actividades/Actividad 5/src/main.js
@@ -78,3 +78,30 @@ function ejemplo6_pag11(){
     document.write('el mayor es '+num2);
     }
 }
+
+function ejemplo7_pag15(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+
+    //Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+    if (pro>=7) {
+        document.write('aprobado');
+    }
+    else {
+        if (pro>=4) {
+            document.write('regular');
+        }
+        else {
+            document.write('reprobado');
+        }
+    }    
+}
