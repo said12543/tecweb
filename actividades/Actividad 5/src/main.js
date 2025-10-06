e57dffdac5d99137084597e250b23e70cac95882
PridedGnu <saidcesar118@gmail.com>
2025-10-05 20:52:08 -0600

    Actividad 5, ejercicio 3

--- actividades/Actividad 5/src/main.js
+++ actividades/Actividad 5/src/main.js
@@ -29,3 +29,15 @@ function ejemplo2_pag6()
     document.write( '<br>' );
     document.write( casado );
 }
+function ejemplo3_pag12()
+{
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
