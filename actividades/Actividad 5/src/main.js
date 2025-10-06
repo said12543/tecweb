1260420d68982ae38007adf5a01603937abe9b6b
PridedGnu <saidcesar118@gmail.com>
2025-10-05 21:26:48 -0600

    Actividad 5, ejercicio 8

--- actividades/Actividad 5/src/main.js
+++ actividades/Actividad 5/src/main.js
@@ -105,3 +105,23 @@ function ejemplo7_pag15(){
         }
     }    
 }
+
+function ejemplo8_pag18(){
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '' );
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+        case 1: document.write('uno');
+        break;
+        case 2: document.write('dos');
+        break;
+        case 3: document.write('tres');
+        break;
+        case 4: document.write('cuatro');
+        break;
+        case 5: document.write('cinco');
+        break;
+        default:document.write('debe ingresar un valor comprendido entre 1 y 5.');
+    }
+}
