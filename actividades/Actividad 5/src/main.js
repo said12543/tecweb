33c7761ec36a76ec7a1f8a679d8e62d70174d2f0
PridedGnu <saidcesar118@gmail.com>
2025-10-05 21:17:32 -0600

    Actividad 5, ejercicio 6

--- actividades/Actividad 5/src/main.js
+++ actividades/Actividad 5/src/main.js
@@ -64,3 +64,17 @@ function ejemplo5_pag8(){
         document.write(nombre+' esta aprobado con un '+nota);
     }
 }
+
+function ejemplo6_pag11(){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+    document.write('el mayor es '+num1);
+    }
+    else {
+    document.write('el mayor es '+num2);
+    }
+}
