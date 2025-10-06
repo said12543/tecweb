ec71e67473fc1da6725ccc91d342a11a9099f162
PridedGnu <saidcesar118@gmail.com>
2025-10-05 20:26:30 -0600

    Actividad 5, ejercicio 1

---
+++ actividades/Actividad 5/src/main.js
@@ -0,0 +1,17 @@
+
+function getDatos()
+{
+    var nombre = prompt("Nombre: ", "");
+
+    var edad = prompt("Edad: ", 0);
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
+
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+}
+function ejemplo1_pag8()
+{
+    document.write('Hola Mundo');
+}
